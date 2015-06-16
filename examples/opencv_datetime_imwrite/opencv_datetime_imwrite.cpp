58b5e6692fa4ed255abe522d7fb32eca143d1abb
Peter Polidoro <peterpolidoro@gmail.com>
2015-06-16 11:15:19 -0400

    Added code for measuring fps.

--- examples/opencv_datetime_imwrite/opencv_datetime_imwrite.cpp
+++ examples/opencv_datetime_imwrite/opencv_datetime_imwrite.cpp
@@ -52,15 +52,18 @@ int main(int argc, char *argv[])
     return -1;
   }
 
+  // Create base directory
   boost::filesystem::path output_path_base(argv[1]);
   createDirectory(output_path_base);
 
+  // Create directory for saving images
   boost::posix_time::ptime date_time = boost::posix_time::second_clock::local_time();
   std::string date_time_string = boost::posix_time::to_iso_string(date_time);
   boost::filesystem::path output_dir(date_time_string);
   boost::filesystem::path output_path = output_path_base / output_dir;
   createDirectory(output_path);
 
+  // Save information about run into file
   boost::filesystem::path run_info_file_path("run_info");
   boost::filesystem::path run_info_file_path_full = output_path_base / run_info_file_path;
   std::ofstream run_info_file;
@@ -68,12 +71,14 @@ int main(int argc, char *argv[])
   run_info_file << output_path << std::endl;
   run_info_file.close();
 
+  // Setup image parameters
   std::vector<int> compression_params;
   compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
   compression_params.push_back(0);
 
   char key = 0;
   int color = 0;
+  boost::posix_time::ptime start_time = boost::posix_time::second_clock::local_time();
   while(key != 'q')
   {
     cv::Mat image = cv::Mat(100,100,CV_8UC3,cv::Scalar(color,color,255));
@@ -83,8 +88,9 @@ int main(int argc, char *argv[])
       color = 0;
     }
     cv::imshow("image",image);
-    key = cv::waitKey(30);
+    key = cv::waitKey(1);
 
+    // Create full image output path
     date_time = boost::posix_time::microsec_clock::local_time();
     std::string time_string = boost::posix_time::to_iso_string(date_time);
     std::ostringstream image_file_name;
@@ -93,7 +99,34 @@ int main(int argc, char *argv[])
     boost::filesystem::path output_file_name_path(image_file_name_string);
     boost::filesystem::path output_path_full = output_path / output_file_name_path;
 
+    // Write image to file
     cv::imwrite(output_path_full.string(),image,compression_params);
   }
+  boost::posix_time::ptime stop_time = boost::posix_time::second_clock::local_time();
+  boost::posix_time::time_duration run_duration = stop_time - start_time;
+  int run_duration_seconds = run_duration.total_seconds();
+  std::cout << "Run duration: " << run_duration_seconds << std::endl;
+
+  int image_count = 0;
+  boost::filesystem::directory_iterator end_iter;
+  for (boost::filesystem::directory_iterator dir_itr(output_path);
+       dir_itr != end_iter;
+       ++dir_itr )
+  {
+    try
+    {
+      if (boost::filesystem::is_regular_file(dir_itr->status()))
+      {
+        ++image_count;
+        // std::cout << dir_itr->path().filename() << "\n";
+      }
+    }
+    catch (const std::exception & ex)
+    {
+      std::cout << dir_itr->path().filename() << " " << ex.what() << std::endl;
+    }
+  }
+  std::cout << "Image count: " << image_count << std::endl;
+  std::cout << "Images per second: " << image_count/run_duration_seconds << std::endl;
   return 0;
 }
